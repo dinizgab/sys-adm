2000349363ae17578fd34633e464d9524e4a4bf8
Gabriel Diniz <gabrielpombodiniz@gmail.com>
2024-05-10 10:51:05 -0300

    feat: creating monitoring_script

---
+++ monitoring_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MACHINE_1_IP="192.168.1.10"
+MACHINE_2_IP="192.168.1.20"
+PORT=22
+
+# Send a single ping with a 1 second timeout
+#
+if ! nc -4 -d -z -w 1 ${MACHINE_1_IP} ${PORT} &> /dev/null; then
+    ansible-playbook playbook_vm1.yaml -i inventory.ini   
+fi
+
+if ! nc -4 -d -z -w 1 ${MACHINE_2_IP} ${PORT} &> /dev/null; then
+   ansible-playbook playbook_vm2.yaml -i inventory.ini
+fi
